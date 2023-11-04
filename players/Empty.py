9cf4b3543a999e6c323c2bcf73d4f4087f446606
Aayush Shukla <oreus.aks@gmail.com>
2023-11-04 23:39:57 +0530

    Fixed unnecessary override

--- players/Empty.py
+++ players/Empty.py
@@ -1,7 +1,5 @@
-import Letter
-import defs.Representation as representation
+import players.Letter as Letter
 
 class Empty(Letter.Letter):
     def __init__(self, row: int, col: int) -> None:
         super().__init__(row, col)
-        self.representation = representation.EMPTY
