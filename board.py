d596d605f604dd8594fb9e51d3a0b71523d476ab
Aayush Shukla <oreus.aks@gmail.com>
2023-11-05 03:24:09 +0530

    Fixed the bug in board not being able to get the representation due to not initializing the class

--- board.py
+++ board.py
@@ -1,5 +1,12 @@
-import players
+import players.Empty
+
 
 class Board:
     def __init__(self) -> None:
-        return
+        self.board = [[players.Empty.Empty(row, col) for col in range(3)] for row in range(3)]
+
+    def display_board(self) -> None:
+        for row in self.board:
+            for player in row:
+                print(player.get_representation(), end=" | ")
+            print()
